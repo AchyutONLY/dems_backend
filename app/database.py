3e3bc40b79d3a2c036b95263e84fd072f370f201
PR202111 <prashant_b240272cs@nitc.ac.in>
2026-03-31 09:46:22 +0530

    added docker support

--- app/database.py
+++ app/database.py
@@ -3,10 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+# use the above if using without password
 
-# DATABASE_URL = "postgresql://postgres:mysecretpassword@db:5432/mydatabase"
-
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{settings.database_username}:"
+    f"{settings.database_password}@"
+    f"{settings.database_hostname}:"
+    f"{settings.database_port}/"
+    f"{settings.database_name}"
+)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False,autocommit = False,bind=engine)
